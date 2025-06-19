b29fdac8ec8fd83afcedee30fbf6645a7e50e3bd
ChrisHIV <15834752+ChrisHIV@users.noreply.github.com>
2025-06-19 20:04:29 +0100

    Additional commit for getting := from data.table not rlang

--- R/mastiff-package.R
+++ R/mastiff-package.R
@@ -1,10 +1,5 @@
 #' @keywords internal
 "_PACKAGE"
 
-## usethis namespace: start
-#' @importFrom rlang :=
-## usethis namespace: end
-NULL
-
 # Declare those dataframe column names we use that generate NOTEs
 utils::globalVariables(c("label", "value", "density_type", "..params_desired"))
