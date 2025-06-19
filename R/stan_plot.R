60f1313988692a1d1c2e48845c96d7e24e15bd83
ChrisHIV <15834752+ChrisHIV@users.noreply.github.com>
2025-06-19 17:41:55 +0100

    Initial commit of stan utils

---
+++ R/stan_plot.R
@@ -0,0 +1,149 @@
+#' Plots the marginal posteriors of each parameter in a stanfit object
+#'
+#' @param stanfit A stanfit object e.g. as returned by `rstan::sampling()`. This
+#'   should contain samples from the posterior.
+#' @param prior_samples A stanfit object containing samples from the prior, so
+#'   that the prior and posterior distributions are overlaid. The default value
+#'   of NA means only the posterior is plotted.
+#' @param true_param_values A named numeric vector: the names are those of
+#'   parameters, and the values are the true values (known from simulation
+#'   truth). Using this argument means the output plot will include vertical
+#'   lines at the true values. The default value of NA means no such lines are
+#'   included.
+#' @param params_desired A character vector of parameter names. Using this
+#'   argument means only named parameters will be included in the plot. The
+#'   default value of NA means all parameters are included.
+#' @param transforms A named list of functions, with the names being parameter
+#'   names. Using this argument means each parameter named in this list will be
+#'   transformed by applying the associated function to its sampled values
+#'   before plotting. e.g. specifying `list(x=log)` would result in the `log`
+#'   function being applied to all values of parameter `"x"`. Note that the
+#'   names of parameters as they appear in the plot are not updated to reflect
+#'   the functions applied, therefore this argument should usually be used
+#'   together with the `labels` argument. The default value of NA means no
+#'   transforms are applied to any parameter.
+#' @param labels A named character vector, with the names being the names of
+#'   parameters as they are named within the stanfit object, and the values
+#'   being new names to use instead. e.g. if `transforms=list(x=log)` were
+#'   specified, `labels(x="log(x)"` would make sense, so that the label of the
+#'   plot of the posterior for x reflects the log transformation.
+#'
+#' @returns A ggplot object.
+#' @export
+plot_posterior <- function( stanfit,
+                            prior_samples = NA,
+                            true_param_values = NA,
+                            params_desired = NA,
+                            transforms = NA,
+                            labels = NA ) {
+
+  # Check args
+  dt_posterior <- stanfit_to_dt( stanfit, params_desired )
+  params <- names( dt_posterior )
+  have_prior <- ! identical(NA, prior_samples )
+  have_true_param_values <- ! identical(NA, true_param_values )
+  have_params_desired <- ! identical(NA, params_desired )
+  have_transforms <- ! identical(NA, transforms )
+  have_labels <- ! identical(NA, labels )
+  if ( have_prior ) {
+    dt_prior <- stanfit_to_dt( prior_samples, params_desired )
+    stopifnot( identical( sort( params ),
+                          sort( names( dt_prior ) ) ) )
+  }
+  if ( have_true_param_values ) {
+    stopifnot( is.numeric( true_param_values ) )
+    unexpected_true_params <- names( true_param_values )[
+      ! names( true_param_values ) %in% params ]
+    if ( length( unexpected_true_params ) ) {
+      warning( paste( "Ignoring the following params which had true values",
+                      "specified, but were not found in the posterior samples:",
+                      paste( unexpected_true_params, collapse = " " ) ) )
+      true_param_values <-
+        true_param_values[ names( true_param_values ) %in% params ]
+      if ( ! length(true_param_values) ) have_true_param_values <- FALSE
+    }
+  }
+  if ( have_transforms ) {
+    stopifnot( is.list ( transforms ) )
+    for ( transform in transforms ) stopifnot( is.function( transform ) )
+    stopifnot( all( names( transforms ) %in% params ) )
+  }
+  if ( have_labels ) {
+    stopifnot( is.character ( labels ) )
+    stopifnot( all( names( labels ) %in% params ) )
+  }
+
+  # Bind posterior and prior if desired
+  dt_posterior$density_type <- "posterior"
+  if ( have_prior ) {
+    dt_prior$density_type <- "prior"
+    dt <- rbind( dt_posterior, dt_prior )
+  } else {
+    dt <- dt_posterior
+  }
+
+  # Transform if desired
+  if ( have_transforms ) {
+    for ( param in names( transforms ) ) {
+      dt[[ param ]] <- transforms[[ param ]]( dt[[ param ]] )
+    }
+    if ( have_true_param_values ) {
+      for ( param in names( transforms ) ) {
+        true_param_values[[ param ]] <-
+          transforms[[ param ]]( true_param_values[[ param ]] )
+      }
+    }
+  }
+
+  # Pivot from wide to long.
+  dt <- data.table::melt( dt,
+                          id.vars = "density_type",
+                          variable.name = "param",
+                          variable.factor = FALSE )
+  if ( have_true_param_values ) {
+    dt_true <- data.table::data.table( param = names( true_param_values ),
+                                       value = true_param_values )
+  }
+
+  # Rename if desired
+  # TODO: for a large data.table it would be noticebly faster to rename the
+  # params as column names in the original wide data.table, than as values
+  # of the single param column after our pivot from wide to long.
+  # But doing that robustly (e.g. imagine labels = c(x="y", y="x")) is tricky
+  if ( have_labels ) {
+    dt_label <- data.table::data.table( param = names( labels ),
+                                        label = labels )
+    dt[ dt_label, on = 'param', label := label]
+    dt[ , param := ifelse( is.na ( label ), param, label ) ]
+    if ( have_true_param_values ) {
+      dt_true[ dt_label, on = 'param', label := label ]
+      dt_true[ , param := ifelse( is.na ( label ), param, label ) ]
+    }
+  }
+
+  plot <- ggplot2::ggplot( dt ) +
+    ggplot2::facet_wrap( ~ param, scales = "free" ) +
+    ggplot2::theme_classic() +
+    ggplot2::coord_cartesian( expand = FALSE )
+  if ( have_prior ) {
+    plot <- plot +
+      ggplot2::geom_histogram( ggplot2::aes( value, fill = density_type ),
+                               bins = 30,
+                               position = "identity",
+                               alpha = 0.6 ) +
+      ggplot2::labs( y = "probability density",
+                     fill = "distribution" ) +
+      ggplot2::scale_fill_brewer( palette = "Set1" )
+  } else {
+    plot <- plot +
+      ggplot2::geom_histogram( ggplot2::aes( value ),
+                               bins = 30 ) +
+      ggplot2::labs( y = "posterior density" )
+  }
+  if ( have_true_param_values ) {
+    plot <- plot +
+      ggplot2::geom_vline( data = dt_true,
+                           ggplot2::aes( xintercept = value ) )
+  }
+  plot
+}
---
+++ R/stan_summaries.R
@@ -0,0 +1,167 @@
+#' Converts a stanfit object to a matrix or data.table
+#'
+#' @param stanfit A stanfit object e.g. as returned by `rstan::sampling()`.
+#' @param params_desired A character vector with the names of those parameters
+#'   in the stanfit object that should be included in the output. The default
+#'   value of `NA` means all parameters are included except `"lp__"` (which is not
+#'   actually a parameter: Stan uses it to record the value of the target
+#'   probability density).
+#'
+#' @returns A matrix or data.table with one column per parameter and one row per
+#'   sample.
+#' @export
+stanfit_to_matrix <- function( stanfit,
+                               params_desired = NA ) {
+  stopifnot( class( stanfit )[[ 1 ]] == "stanfit" )
+  stanfit_as_matrix <- as.matrix( stanfit )
+  if ( ! identical(NA, params_desired ) ) {
+    stopifnot( is.character( params_desired ) )
+    stopifnot( length( params_desired ) > 0L )
+    for (param in params_desired) {
+      if (! param %in% colnames( stanfit_as_matrix ) ) {
+        stop(paste("Parameter", param, "not present in stanfit object"))
+      }
+    }
+    stanfit_as_matrix <- stanfit_as_matrix[ , params_desired, drop = FALSE ]
+  } else {
+    stanfit_as_matrix <-
+      stanfit_as_matrix[ , colnames( stanfit_as_matrix ) != "lp__", drop = FALSE ]
+  }
+  stanfit_as_matrix
+}
+
+#' @rdname stanfit_to_matrix
+#' @export
+stanfit_to_dt <- function( stanfit,
+                           params_desired = NULL ) {
+  stopifnot( class( stanfit )[[ 1 ]] == "stanfit" )
+  stanfit_as_dt <- data.table::as.data.table( stanfit )
+  if ( ! identical(NA, params_desired ) ) {
+    stopifnot( is.character( params_desired ) )
+    stopifnot( length( params_desired ) > 0L )
+    for (param in params_desired) {
+      if (! param %in% colnames( stanfit_as_dt ) ) {
+        stop(paste("Parameter", param, "not present in stanfit object"))
+      }
+    }
+    stanfit_as_dt <- stanfit_as_dt[ , ..params_desired ]
+  } else {
+    stanfit_as_dt <-
+      stanfit_as_dt[ , colnames( stanfit_as_dt ) != "lp__", with = FALSE ]
+  }
+  stanfit_as_dt
+}
+
+#' Calculates the mean or median for each parameter for each stanfit object
+#'
+#' In presto the typical list supplied to this would have one stanfit object
+#' for each simulated trial.
+#'
+#' @param stanfit_list A list of stanfit objects e.g. as returned by
+#'   `rstan::sampling()`. The function's name reflects the assumption that these
+#'   contain samples from the posterior, but they could contain samples from the
+#'   prior.
+#' @param params_desired A character vector with the names of those parameters
+#'   in the stanfit object that should be included in the output. The default
+#'   value of `NA` means all parameters are included except `"lp__"` (which is not
+#'   actually a parameter: Stan uses it to record the value of the target
+#'   probability density).
+#'
+#' @returns A 2D array: the first index for the parameter, the second index for
+#    the stanfit object.
+#' @export
+posterior_means <- function( stanfit_list,
+                             params_desired = NA ) {
+  stopifnot( is.list( stanfit_list ) )
+  results_list <- lapply( stanfit_list, function( samples_one_stanfit ) {
+    samples_one_stanfit <-
+      stanfit_to_matrix( samples_one_stanfit, params_desired )
+    apply( samples_one_stanfit, 2, mean )
+  } )
+  simplify2array( results_list )
+}
+
+#' @rdname posterior_means
+#' @export
+posterior_medians <- function( stanfit_list,
+                               params_desired = NA ) {
+  stopifnot( is.list( stanfit_list ) )
+  results_list <- lapply( stanfit_list, function( samples_one_stanfit ) {
+    samples_one_stanfit <-
+      stanfit_to_matrix( samples_one_stanfit, params_desired )
+    apply( samples_one_stanfit, 2, stats::median )
+  } )
+  simplify2array( results_list )
+}
+
+#' Calculates central probability intervals for each parameter for each stanfit
+#' object
+#'
+#' The function is a simple wrapper around `rstantools::posterior_interval()`.
+#' In presto the typical list supplied to this would have one stanfit object
+#' for each simulated trial.
+#'
+#' @param stanfit_list A list of stanfit objects e.g. as returned by
+#'   `rstan::sampling()`. The function's name reflects the assumption that these
+#'   contain samples from the posterior, but they could contain samples from the
+#'   prior.
+#' @param prob A single number between 0 and 1 specifying the amount of
+#'   probability to be contained in the interval. e.g. Specifying 0.95 results
+#'   in an interval that spans the 2.5th to 97.5th percentiles of the
+#'   distribution.
+#' @param params_desired A character vector with the names of those parameters
+#'   in the stanfit object that should be included in the output. The default
+#'   value of `NA` means all parameters are included except `"lp__"` (which is
+#'   not actually a parameter: Stan uses it to record the value of the target
+#'   probability density).
+#' @param ... Additional arguments to pass to
+#'   `rstantools::posterior_interval()`.
+#'
+#' @returns A 3D array: the first index for the parameter, the second for the
+#'   percentile, the third for the stanfit object.
+#' @export
+posterior_intervals <- function( stanfit_list,
+                                 prob,
+                                 params_desired = NA,
+                                 ... ) {
+  stopifnot( is.numeric( prob ) )
+  stopifnot( length( prob ) == 1 )
+  stopifnot( prob >= 0 )
+  stopifnot( prob <= 1 )
+  stopifnot( is.list( stanfit_list ) )
+  results_list <- lapply( stanfit_list, function( samples_one_stanfit ) {
+    samples_one_stanfit <-
+      stanfit_to_matrix( samples_one_stanfit, params_desired )
+    rstantools::posterior_interval( samples_one_stanfit,
+                                    prob = prob,
+                                    ... )
+  } )
+  simplify2array( results_list )
+}
+
+#' Calculates the amount of probability (mass) a parameter has in a range
+#'
+#' @param stanfit A stanfit object e.g. as returned by `rstan::sampling()`. The
+#'   function's name reflects the assumption that this contains samples from the
+#'   posterior, but it could contain samples from the prior.
+#' @param param The name of the parameter, as a character.
+#' @param range The lower and upper boundaries of the range, as a length-2
+#'   numeric vector. The boundaries are not considered part of the range (this
+#'   should be irrelevant for continuous parameters). The lower boundary can be
+#'   -Inf for a range that is unbounded from below; the upper boundary may be
+#'   Inf for a range that is unbounded from above.
+#'
+#' @returns A single number: the amount of probability (mass) that this
+#'   parameter has in this range, estimated as the fraction of samples in the
+#'   stanfit object for which the parameter is in the range.
+#' @export
+posterior_mass_in_range <- function( stanfit, param, range ) {
+  stopifnot( class( stanfit )[[ 1 ]] == "stanfit" )
+  stopifnot( is.character( param ) )
+  stopifnot( length( param ) == 1 )
+  stopifnot( is.numeric( range ) )
+  stopifnot( length( range ) == 2 )
+  stopifnot( range[[1]] <= range[[2]] )
+  m <- stanfit_to_matrix( stanfit, params_desired = param )
+  mean( m[ , param ] > range[[ 1 ]] & m[ , param ] < range[[ 2 ]] )
+}
