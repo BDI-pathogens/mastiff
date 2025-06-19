9aa69b75aa4cd7ecc4f3786bc9a89755774075fd
ChrisHIV <15834752+ChrisHIV@users.noreply.github.com>
2025-06-19 17:46:50 +0100

    Initial commit of docs

---
+++ R/mastiff-package.R
@@ -0,0 +1,10 @@
+#' @keywords internal
+"_PACKAGE"
+
+## usethis namespace: start
+#' @importFrom rlang :=
+## usethis namespace: end
+NULL
+
+# Declare those dataframe column names we use that generate NOTEs
+utils::globalVariables(c("label", "value", "density_type", "..params_desired"))
