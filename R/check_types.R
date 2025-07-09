636d6286cbbc34cf2c21d7a2b44070dc6db62313
ChrisHIV <15834752+ChrisHIV@users.noreply.github.com>
2025-07-09 10:46:49 +0100

    Finished check_numeric and check_logical funcs

--- R/check_types.R
+++ R/check_types.R
@@ -1,4 +1,37 @@
-# TODO: document
+
+#' Checks a variable is a single number (and optionally, in a range and not NA)
+#'
+#' @param x The variable to check.
+#' @param lower The lower limit for `x` (default = `-Inf`).
+#' @param lower_inclusive Whether the lower limit itself is inclusive, i.e.
+#'   whether we require `x >= lower` or `x > lower` (the default is the former).
+#' @param upper The upper limit for `x` (default = `Inf`).
+#' @param upper_inclusive Whether the upper limit itself is inclusive, i.e.
+#'   whether we require `x <= upper` or `x < lower` (the default is the former).
+#' @param allow_missing Whether x is allowed to be missing (default = `FALSE`).
+#'   If `allow_missing` is `TRUE` we skip the tests comparing `x` to `lower` and
+#'   `upper`.
+#'
+#' @returns `invisible(TRUE)`, if no problems were found; otherwise a
+#'   `stopifnot()` is triggered.
+#' @export
+#'
+#' @examples
+#' # These are all OK:
+#' check_numeric(0)
+#' check_numeric(0L)
+#' check_numeric(0, lower = 0)
+#' check_numeric(0, upper = 0)
+#' check_numeric(NA_real_, allow_missing = TRUE)
+#' check_numeric(Inf)
+#' check_numeric(-Inf)
+#' # These return errors:
+#' try(check_numeric("foo"))
+#' try(check_numeric(1:2))
+#' try(check_numeric(NA_real_))
+#' try(check_numeric(NA, allow_missing = TRUE)) # NA is logical, not numeric
+#' try(check_numeric(0, lower = 1))
+#' try(check_numeric(0, lower = 0, lower_inclusive = FALSE))
 check_numeric <- function(x,
                           lower = -Inf,
                           lower_inclusive = TRUE,
@@ -25,23 +58,44 @@ check_numeric <- function(x,
   # Then do the checks the function is designed for
   stopifnot(is.numeric(x))
   stopifnot(length(x) == 1)
-  if (! allow_missing) stopif(is.na(x))
-  if (lower_inclusive) {
-    stopifnot(lower <= x)
-  } else {
-    stopifnot(lower < x)
-  }
-  if (upper_inclusive) {
-    stopifnot(x <= upper)
-  } else {
-    stopifnot(x < upper)
+  if (! allow_missing) {
+    stopifnot(! is.na(x))
+    if (lower_inclusive) {
+      stopifnot(lower <= x)
+    } else {
+      stopifnot(lower < x)
+    }
+    if (upper_inclusive) {
+      stopifnot(x <= upper)
+    } else {
+      stopifnot(x < upper)
+    }
   }
 
-  # TODO: check how a missing value behaves with respect to lower and upper constraints
+  invisible(TRUE)
 
 }
 
-# TODO: document
+
+#' Checks a variable is a single logical value (and optionally not missing)
+#'
+#' @param x The variable to check.
+#' @param allow_missing Whether x is allowed to be missing (default = `FALSE`).
+#'
+#' @returns `invisible(TRUE)`, if no problems were found; otherwise a
+#'   `stopifnot()` is triggered.
+#' @export
+#'
+#' @examples
+#' # These are all OK:
+#' check_logical(TRUE)
+#' check_logical(FALSE)
+#' check_logical(NA, allow_missing = TRUE)
+#' # These return errors:
+#' try(check_logical("foo"))
+#' try(check_logical(c(TRUE, TRUE)))
+#' try(check_logical(NA))
+#' try(check_logical(NA_real_, allow_missing = TRUE)) # NA_real_ is not logical
 check_logical <- function(x, allow_missing = FALSE) {
 
   # First check this function was called correctly
@@ -53,6 +107,8 @@ check_logical <- function(x, allow_missing = FALSE) {
   # Then do the checks the function is designed for
   stopifnot(is.logical(x))
   stopifnot(length(x) == 1)
-  if (! allow_missing) stopif(is.na(x))
+  if (! allow_missing) stopifnot(! is.na(x))
+
+  invisible(TRUE)
 
 }
