030a8d7bc0a53aafd5d64b20fb7b1c36599c8a91
ChrisHIV <15834752+ChrisHIV@users.noreply.github.com>
2025-07-21 16:37:42 +0100

    Added tests for estimation

--- tests/testthat/test-mixture_of_two_normals.R
+++ tests/testthat/test-mixture_of_two_normals.R
@@ -1,3 +1,5 @@
+run_slow_estimation_test <- TRUE
+
 test_that("simulating with default params gives no error", {
   expect_no_error(simulate_mixture_of_two_normals())
 })
@@ -102,3 +104,60 @@ test_that("simulating a one-component mixture with no width gives a Dirac delta 
   mu_1 <- 10
   expect_true(all(simulate_mixture_of_two_normals(p = 1, sd_1 = 0, mu_1 = mu_1)$y == mu_1))
 })
+
+test_that("estimation with multiple groups has the right shape output", {
+  groups <- c("A", "B", "C", "D") # Can change to something else for testing
+  num_groups_in_output <- 0
+  while (num_groups_in_output < length(groups)) {
+    results <- simulate_mixture_of_two_normals(n = length(groups),
+                                               groups = groups)
+    num_groups_in_output <- length(unique(results$data$group))
+  }
+  prior_boundaries <- data.frame(
+    param = c("mu_0", "mu_1", "sd_0", "sd_1", "sd_groups", "p"),
+    lower = c(    -1,      9,      0,      0,           0,   0),
+    upper = c(     1,     11,   0.02,   0.02,         0.01,  1)
+  )
+  # Supress warnings from non-converging short MCMC
+  posterior <- suppressWarnings(estimate_mixture_of_two_normals(
+    y = results$data$y,
+    groups = results$data$group,
+    prior_boundaries = prior_boundaries,
+    iter = 2,
+    chains = 1))
+  expect_true(is.data.frame(posterior))
+  expect_equal(sort(names(posterior)),
+               c("mu_0", "mu_1", "p", paste0("p_for_", groups),
+                 paste0("prob_is_1[", 1:length(groups), "]"), "sd_0", "sd_1",
+                 "sd_groups"))
+})
+
+if (run_slow_estimation_test) {
+  test_that("estimation of two narrow normals is accurate", {
+    mu_0 <- 0
+    mu_1 <- 10
+    sd_ <- 0.01
+    n_each <- 50
+    results <- simulate_mixture_of_two_normals(n = n_each * 2,
+                                               mu_0 = mu_0,
+                                               mu_1 = mu_1,
+                                               sd_0 = sd_,
+                                               sd_1 = sd_,
+                                               p = 0.5,
+                                               groups = character())
+    prior_boundaries <- data.frame(
+      param = c("mu_0", "mu_1", "sd_0", "sd_1", "sd_groups", "p"),
+      lower = c(    -1,      9,      0,      0,           0,   0),
+      upper = c(     1,     11,   0.02,   0.02,         0.01,  1)
+    )
+    posterior <- estimate_mixture_of_two_normals(y = results$data$y,
+                                                 prior_boundaries = prior_boundaries)
+    tolerance <- 0.01 # the allowed absolute error in the posterior mean
+    # The expected failure probability, given the sampling distribution of the
+    # mean of n_each samples, is 2 * pnorm(-tolerance, sd = sd_ / sqrt(n_each))
+    # Though n_each for the d=0 and d=1 categories is not fixed but is
+    # ~ Binom(2 * n_each, p).
+    expect_lt(abs(mean(posterior$mu_0) - mu_0), tolerance)
+    expect_lt(abs(mean(posterior$mu_1) - mu_1), tolerance)
+  })
+}
