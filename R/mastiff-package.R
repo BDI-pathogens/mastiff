a22cb4ec0fe7c19b2c76b5246025360ba80a5091
ChrisHIV <15834752+ChrisHIV@users.noreply.github.com>
2025-10-21 17:18:01 +0100

    Housekeeping for R CMD check: declare 'density' globally

--- R/mastiff-package.R
+++ R/mastiff-package.R
@@ -2,7 +2,7 @@
 "_PACKAGE"
 
 # Declare those dataframe column names we use that generate NOTEs
-utils::globalVariables(c("label", "value", "density_type", "..params_desired"))
+utils::globalVariables(c("label", "value", "density_type", "..params_desired", "density"))
 
 #' The 'mastiff' package.
 #'
