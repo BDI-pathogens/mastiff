e64174baf30c710e2ef52b0e524b16b0134bcf49
Ian Roberts <ian.roberts@ndm.ox.ac.uk>
2026-09-25 16:44:04 +0100

    Adding truncated exponential documentation and temporarily removing truncated normal

--- R/distribution_truncated_factory.R
+++ R/distribution_truncated_factory.R
@@ -62,7 +62,7 @@ distribution.truncated.exponential.class <- R6.class(
     ############################################################################/
     # standard deviation
     ############################################################################/
-    #' @field mean The standard deviation of an exponential distribution with
+    #' @field sd The standard deviation of an exponential distribution with
     #'   rate `$params$rate` truncated to `[$t0, $t1]`.
     sd = function(val) {
       if (!missing(val)) {
@@ -73,7 +73,7 @@ distribution.truncated.exponential.class <- R6.class(
     ############################################################################/
     # variance
     ############################################################################/
-    #' @field mean The variance of an exponential distribution with rate
+    #' @field var The variance of an exponential distribution with rate
     #'   `$params$rate` truncated to `[$t0, $t1]`.
     var = function(val) {
       if (!missing(val)) {
@@ -102,29 +102,4 @@ distribution.truncated.exponential.class <- R6.class(
 #' @export
 distribution.truncated.exponential <- function(rate = 1, t0 = 0, t1 = Inf) {
   distribution.truncated.exponential.class$new(rate = rate, t0 = t0, t1 = t1)
-}
-
-################################################################################/
-# Truncated normal distribution
-################################################################################/
-#' distribution.truncated.normal.class
-distribution.truncated.normal.class <- distribution_truncated_factory(
-  classname = "distribution.truncated.normal",
-  untruncated_distribution = distribution.normal()
-)
-
-#' distribution.truncated.normal
-#'
-#' Constructor function for a random variable distributed under a truncated
-#' normal distribution.
-#'
-#' @inheritParams distribution.normal mean sd
-#' @param t0,t1 Lower and upper limits for truncation
-#'
-#' @returns An object of class [distribution.truncated.normal.class]
-#' @export
-distribution.truncated.normal <- function(mean = 0, sd = 1, t0 = 0, t1 = Inf) {
-  x <- distribution.truncated.normal.class$new(t0 = t0, t1 = t1)
-  x$params <- list(mean = mean, sd = sd)
-  return(x)
 }
