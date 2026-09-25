628ef8fb3ec65e2f8b30492c3b9d47343d6f9643
Ian Roberts <ian.roberts@ndm.ox.ac.uk>
2026-09-25 16:41:51 +0100

    Removing factory function and defining truncated exponential class manually

--- R/distribution_truncated_factory.R
+++ R/distribution_truncated_factory.R
@@ -1,73 +1,93 @@
-# distribution_truncated_factory
-#
-# Factory function to generate truncated distribution classes
-#
-# @param classname The classname for the derived truncated distribution class
-# @param untruncated_distribution Mastiff R6 distribution class giving the
-#   untruncated version of the distribution
-# @param func_mean Function to evaluate the mean of the distribution using the
-#   R6 structure of the truncated distribution. The default evaluates the mean
-#   using numerical integration, but can be overwritten if an analytic form is
-#   known
-# @param func_sd Function to evaluate the standard deviation of the distribution
-#   using the R6 structure of the truncated distribution. The default evaluates
-#   the standard deviation as the square root of the variance, but can be
-#   overwritten if an analytic form is known
-# @param func_var Function to evaluate the variance of the distribution using
-#   the R6 structure of the truncated distribution. The default evaluates the
-#   variance using numerical integration, but can be overwritten if an analytic
-#   form is known
-#
-# @returns A R6 class generator which can be initialised into a full R6
-#   truncated distribution class
-
-distribution_truncated_factory <- function(
-    classname,
-    untruncated_distribution,
-    func_mean = function(self) integrate(function(x) x * self$d(x),
-                                         lower = self$support[1],
-                                         upper = self$support[2])$value,
-    func_sd = function(self) sqrt(self$var),
-    func_var = function(self){
-      mu <- self$mean
-      integrate(function(x) (x - mu)^2 * self$d(x),
-                lower = self$support[1],
-                upper = self$support[2])$value
-    }
-){
-  R6.class(
-    classname = classname,
-    inherit = distribution.truncated.class,
-    private = list(
-      .distribution = untruncated_distribution
-    ),
-    active = list(
-      mean = function(val){
-        if (!missing(val))
-          stop("`$mean()` cannot be set.")
-        func_mean(self)
-      },
-      sd = function(val){
-        if (!missing(val))
-          stop("`$sd()` cannot be set.")
-        func_sd(self)
-      },
-      var = function(val){
-        if (!missing(val))
-          stop("`$var()` cannot be set.")
-        func_var(self)
-      }
-    )
-  )
-}
-
 ################################################################################/
 # Truncated exponential distribution
 ################################################################################/
-#' distribution.truncated.exponential.class
-distribution.truncated.exponential.class <- distribution_truncated_factory(
-  classname = "distribution.truncated.exponential",
-  untruncated_distribution = distribution.exponential()
+#' Class: `distribution.truncated.exponential.class`
+#' @description Derived class for an exponentially-distributed random variable
+#'   with interval truncation.
+distribution.truncated.exponential.class <- R6.class(
+  classname = "distribution.truncated.exponential.class",
+  inherit = distribution.truncated.class,
+  private = list(
+    .name = "TruncatedExponential",
+    .param_names = c("rate"),
+    .check_params = function(params) {
+      # Check that params contains all elements of private$.param_names
+      super$.check_params(params)
+
+      # Check that params$rate is a non-negative numeric value
+      if (!is.numeric(params$rate)) {
+        stop("`params$rate` must be a numeric value.")
+      }
+      if (params$rate < 0) {
+        stop("`params$rate` must be >0.")
+      }
+
+      return(params)
+    }
+  ),
+  public = list(
+    ############################################################################/
+    # initialize
+    ############################################################################/
+    #' @description Create a new object of class `distribution.truncated.exponential.class`
+    #' 
+    #' @param rate The rate of the exponential distribution
+    #' @param t0 Lower bound for truncation
+    #' @param t1 Upper bound for truncation
+    initialize = function(rate = 1, t0 = 0, t1 = Inf) {
+      super$initialize(
+        distribution.exponential(rate = rate, offset = 0),
+        t0 = t0,
+        t1 = t1
+      )
+    }
+  ),
+  active = list(
+    ############################################################################/
+    # mean
+    ############################################################################/
+    #' @field mean The mean of an exponential distribution with rate
+    #'   `$params$rate` truncated to `[$t0, $t1]`.
+    mean = function(val) {
+      if (!missing(val)) {
+        stop("cannot set `$mean`")
+      }
+
+      t0 <- private$.t0
+      t1 <- private$.t1
+      lambda <- private$.params$rate
+
+      return(t0 + 1 / lambda - (t1 - t0) / (exp(lambda * (t1 - t0)) - 1))
+    },
+    ############################################################################/
+    # standard deviation
+    ############################################################################/
+    #' @field mean The standard deviation of an exponential distribution with
+    #'   rate `$params$rate` truncated to `[$t0, $t1]`.
+    sd = function(val) {
+      if (!missing(val)) {
+        stop("cannot set `$sd`")
+      }
+      return(sqrt(self$var))
+    },
+    ############################################################################/
+    # variance
+    ############################################################################/
+    #' @field mean The variance of an exponential distribution with rate
+    #'   `$params$rate` truncated to `[$t0, $t1]`.
+    var = function(val) {
+      if (!missing(val)) {
+        stop("cannot set `$var`")
+      }
+
+      t0 <- private$.t0
+      t1 <- private$.t1
+      lambda <- private$.params$rate
+      
+      return(1 / lambda^2 - (t1 - t0)^2 * exp(lambda * (t1 - t0)) /
+               (exp(lambda * (t1 - t0)) - 1)^2)
+    }
+  )
 )
 
 #' distribution.truncated.exponential
@@ -77,13 +97,11 @@ distribution.truncated.exponential.class <- distribution_truncated_factory(
 #'
 #' @inheritParams distribution.exponential rate
 #' @param t0,t1 Lower and upper limits for truncation
-#' 
+#'
 #' @returns An object of class [distribution.truncated.exponential.class]
 #' @export
-distribution.truncated.exponential <- function(rate = 1, t0 = 0, t1 = Inf){
-  x <- distribution.truncated.exponential.class$new(t0 = t0, t1 = t1)
-  x$params$rate <- rate
-  return(x)
+distribution.truncated.exponential <- function(rate = 1, t0 = 0, t1 = Inf) {
+  distribution.truncated.exponential.class$new(rate = rate, t0 = t0, t1 = t1)
 }
 
 ################################################################################/
@@ -102,10 +120,10 @@ distribution.truncated.normal.class <- distribution_truncated_factory(
 #'
 #' @inheritParams distribution.normal mean sd
 #' @param t0,t1 Lower and upper limits for truncation
-#' 
+#'
 #' @returns An object of class [distribution.truncated.normal.class]
 #' @export
-distribution.truncated.normal <- function(mean = 0, sd = 1, t0 = 0, t1 = Inf){
+distribution.truncated.normal <- function(mean = 0, sd = 1, t0 = 0, t1 = Inf) {
   x <- distribution.truncated.normal.class$new(t0 = t0, t1 = t1)
   x$params <- list(mean = mean, sd = sd)
   return(x)
