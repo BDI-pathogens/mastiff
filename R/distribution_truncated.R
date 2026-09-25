969eec3d447314685da3878691a7d38510207b4f
Ian Roberts <ian.roberts@ndm.ox.ac.uk>
2026-09-25 16:57:08 +0100

    Adding truncated normal distribution and moving all back into R/distribution_truncated.R

--- R/distribution_truncated.R
+++ R/distribution_truncated.R
@@ -238,3 +238,226 @@ distribution.truncated <- function(distribution,
     )
   )
 }
+
+################################################################################/
+# Truncated exponential distribution
+################################################################################/
+#' Class: `distribution.truncated.exponential.class`
+#' @description Derived class for an exponentially-distributed random variable
+#'   with interval truncation.
+distribution.truncated.exponential.class <- R6.class(
+  classname = "distribution.truncated.exponential.class",
+  inherit = distribution.truncated.class,
+  private = list(
+    .name = "TruncatedExponential",
+    .param_names = c("rate"),
+    .check_params = function(params) {
+      # Check that params contains all elements of private$.param_names
+      super$.check_params(params)
+      
+      # Check that params$rate is a non-negative numeric value
+      if (!is.numeric(params$rate)) {
+        stop("`params$rate` must be a numeric value.")
+      }
+      if (params$rate < 0) {
+        stop("`params$rate` must be >0.")
+      }
+      
+      return(params)
+    }
+  ),
+  public = list(
+    ############################################################################/
+    # initialize
+    ############################################################################/
+    #' @description Create a new object of class `distribution.truncated.exponential.class`
+    #' 
+    #' @param rate The rate of the exponential distribution
+    #' @param t0 Lower bound for truncation
+    #' @param t1 Upper bound for truncation
+    initialize = function(rate = 1, t0 = 0, t1 = Inf) {
+      super$initialize(
+        distribution.exponential(rate = rate, offset = 0),
+        t0 = t0,
+        t1 = t1
+      )
+    }
+  ),
+  active = list(
+    ############################################################################/
+    # mean
+    ############################################################################/
+    #' @field mean The mean of an exponential distribution with rate
+    #'   `$params$rate` truncated to `[$t0, $t1]`.
+    mean = function(val) {
+      if (!missing(val)) {
+        stop("cannot set `$mean`")
+      }
+      
+      t0 <- private$.t0
+      t1 <- private$.t1
+      lambda <- self$params$rate
+      
+      return(t0 + 1 / lambda - (t1 - t0) / (exp(lambda * (t1 - t0)) - 1))
+    },
+    ############################################################################/
+    # standard deviation
+    ############################################################################/
+    #' @field sd The standard deviation of an exponential distribution with
+    #'   rate `$params$rate` truncated to `[$t0, $t1]`.
+    sd = function(val) {
+      if (!missing(val)) {
+        stop("cannot set `$sd`")
+      }
+      return(sqrt(self$var))
+    },
+    ############################################################################/
+    # variance
+    ############################################################################/
+    #' @field var The variance of an exponential distribution with rate
+    #'   `$params$rate` truncated to `[$t0, $t1]`.
+    var = function(val) {
+      if (!missing(val)) {
+        stop("cannot set `$var`")
+      }
+      
+      t0 <- private$.t0
+      t1 <- private$.t1
+      lambda <- self$params$rate
+      
+      return(1 / lambda^2 - (t1 - t0)^2 * exp(lambda * (t1 - t0)) /
+               (exp(lambda * (t1 - t0)) - 1)^2)
+    }
+  )
+)
+
+#' distribution.truncated.exponential
+#'
+#' Constructor function for a random variable distributed under a truncated
+#' exponential distribution.
+#'
+#' @inheritParams distribution.exponential rate
+#' @param t0,t1 Lower and upper limits for truncation
+#'
+#' @returns An object of class [distribution.truncated.exponential.class]
+#' @export
+distribution.truncated.exponential <- function(rate = 1, t0 = 0, t1 = Inf) {
+  distribution.truncated.exponential.class$new(rate = rate, t0 = t0, t1 = t1)
+}
+
+################################################################################/
+# Truncated normal distribution
+################################################################################/
+#' Class: `distribution.truncated.normal.class`
+#' @description Derived class for an normally-distributed random variable
+#'   with interval truncation.
+distribution.truncated.normal.class <- R6.class(
+  classname = "distribution.truncated.normal.class",
+  inherit = distribution.truncated.class,
+  private = list(
+    .name = "TruncatedNormal",
+    .param_names = c("mean", "sd"),
+    .check_params = function( params ){
+      # Check that params contains all elements of private$.param_names
+      super$.check_params( params )
+      
+      if ( !is.numeric( params$mean ) )
+        stop( "`params$mean` must be a numeric value.")
+      if ( params$sd < 0 || !is.numeric( params$sd ) )
+        stop( "`params$sd` must be a non-negative numeric value.")
+      
+      return( params )
+    }
+  ),
+  public = list(
+    ############################################################################/
+    # initialize
+    ############################################################################/
+    #' @description Create a new object of class `distribution.truncated.normal.class`
+    #' 
+    #' @param mean The mean of the untruncated normal distribution.
+    #' @param sd The standard deviation of the untruncated normal distribution.
+    #' @param t0 Lower bound for truncation
+    #' @param t1 Upper bound for truncation
+    initialize = function(mean = 0, sd = 1, t0 = 0, t1 = Inf) {
+      super$initialize(
+        distribution.normal(mean = mean, sd = sd),
+        t0 = t0,
+        t1 = t1
+      )
+    }
+  ),
+  active = list(
+    ############################################################################/
+    # mean
+    ############################################################################/
+    #' @field mean The mean of a normal distribution with mean `$params$mean`
+    #'   and standard deviation `$params$sd` truncated to `[$t0, $t1]`.
+    mean = function(val) {
+      if (!missing(val)) {
+        stop("cannot set `$mean`")
+      }
+      
+      t0 <- private$.t0
+      t1 <- private$.t1
+      mu <- self$params$mean
+      sigma <- self$params$sd
+      
+      alpha <- (t0 - mu) / sigma
+      beta  <- (t1 - mu) / sigma
+      Z     <- pnorm(beta) - pnorm(alpha)
+      
+      return(mu + sigma * (dnorm(alpha) - dnorm(beta)) / Z)
+    },
+    ############################################################################/
+    # standard deviation
+    ############################################################################/
+    #' @field sd The standard deviation of a normal distribution with mean
+    #'   `$params$mean` and standard deviation `$params$sd` truncated to 
+    #'   `[$t0, $t1]`.
+    sd = function(val) {
+      if (!missing(val)) {
+        stop("cannot set `$sd`")
+      }
+      return(sqrt(self$var))
+    },
+    ############################################################################/
+    # variance
+    ############################################################################/
+    #' @field var The variance of a normal distribution with mean `$params$mean`
+    #'   and standard deviation `$params$sd` truncated to `[$t0, $t1]`.
+    var = function(val) {
+      if (!missing(val)) {
+        stop("cannot set `$var`")
+      }
+      
+      t0 <- private$.t0
+      t1 <- private$.t1
+      mu <- self$params$mean
+      sigma <- self$params$sd
+      
+      alpha <- (t0 - mu) / sigma; dalpha <- dnorm(alpha)
+      beta  <- (t1 - mu) / sigma; dbeta  <- dnorm(beta)
+      Z     <- pnorm(beta) - pnorm(alpha)
+      
+      return(sigma^2 * (1 - (beta * dbeta - alpha * dalpha) / Z -
+                          ((dalpha - dbeta) / Z)^2))
+    }
+  )
+)
+
+#' distribution.truncated.normal
+#'
+#' Constructor function for a random variable distributed under a truncated
+#' normal distribution.
+#'
+#' @inheritParams distribution.normal mean sd
+#' @param t0,t1 Lower and upper limits for truncation
+#'
+#' @returns An object of class [distribution.truncated.normal.class]
+#' @export
+distribution.truncated.normal <- function(mean = 0, sd = 1, t0 = 0, t1 = Inf) {
+  distribution.truncated.normal.class$new(
+    mean = mean, sd = sd, t0 = t0, t1 = t1
+  )
+}
